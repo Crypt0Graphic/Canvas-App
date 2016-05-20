e6d78ee304514cff852c4181cfb25e38d235b2d8
zafer.kayikcilar <zafer.kayikcilar@aptech.com.tr>
2016-05-20 15:59:40 +0300

    Bootstrap

--- src/app/canvas-app.component.ts
+++ src/app/canvas-app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CanvasComponent } from './+canvas';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'canvas-app-app',
   templateUrl: 'canvas-app.component.html',
-  styleUrls: ['canvas-app.component.css']
+  styleUrls: ['canvas-app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/canvas', component: CanvasComponent}
+])
 export class CanvasAppAppComponent {
   title = 'Ana Sayfa!';
 }
