4ea26aa3c49d414fdf5f74489ebc5eb65a77bd48
zafer.kayikcilar <zafer.kayikcilar@aptech.com.tr>
2016-05-23 17:49:34 +0300

    MultipleCanvas Images

--- src/app/+canvases/canvases.component.ts
+++ src/app/+canvases/canvases.component.ts
@@ -11,6 +11,22 @@ export class CanvasesComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
+    var canvasFirst = <HTMLCanvasElement>document.getElementById("firstCanvas");
+    var ctxFirst = canvasFirst.getContext("2d");
+    var imgFirst = new Image();
+
+    var canvasSecond = <HTMLCanvasElement>document.getElementById("secondCanvas");
+    var ctxSecond = canvasSecond.getContext("2d");
+    var imgSecond = new Image();
+
+    imgFirst.onload = function() {
+
+      ctxFirst.drawImage(imgFirst, 0, 0);
+      ctxSecond.drawImage(imgSecond, 0, 0);
+    };
+
+    imgFirst.src = './clips/kupurFirst.jpg';
+    imgSecond.src = './clips/kupurSecond.jpg';
   }
 
 }
