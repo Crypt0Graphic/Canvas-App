26bfd6e51a326cec88fc91ba0ab780b15e162d75
zafer.kayikcilar <zafer.kayikcilar@aptech.com.tr>
2016-05-26 08:34:09 +0300

    değişiklik

--- src/app/+fabric/fabric.component.ts
+++ src/app/+fabric/fabric.component.ts
@@ -15,30 +15,38 @@ export class FabricComponent implements OnInit {
 
   ngOnInit() {
     var canvas = new fabric.Canvas('c');
+    var imgElement1 = <HTMLImageElement>document.getElementById('my-image1');
+    var imgElement2 = <HTMLImageElement>document.getElementById('my-image2');
+    
+    var img1 = new fabric.Image(imgElement1,{cornerColor:'Red', borderColor:'Red'});
+    var img2 = new fabric.Image(imgElement2,{cornerColor:'Red'});
+    
+    img1.lockUniScaling = true;
+    img2.lockUniScaling = true;
+    canvas.add(img1, img2);
+    
+    //img1.setCornersize(3);
+    
+    // //Alternatif 1
+    // fabric.Image.fromURL('../../clips/kupurFirst.jpg', function(oImg) {
+    // canvas.add(oImg);
+    // });
+    
+    // canvas.setBackgroundColor('#E5E5E5', canvas.renderAll.bind(canvas));
 
-    //Alternatif 1
-    fabric.Image.fromURL('../../clips/kupurFirst.jpg', function(oImg) {
-    // scale image down, and flip it, before adding it onto canvas
-    canvas.add(oImg);
-    });
+    // canvas.on({
+    // 'object:moving': onChange,
+    // 'object:scaling': onChange,
+    // 'object:rotating': onChange
+    // });
 
-    fabric.Image.fromURL('../../clips/kupurSecond.jpg', function(oImg) {
-    canvas.add(oImg);
-    });
-
-    canvas.on({
-    'object:moving': onChange,
-    'object:scaling': onChange,
-    'object:rotating': onChange,
-    });
-
-     function onChange(options) {
-      options.target.setCoords();
-      canvas.forEachObject(function(obj) {
-        if (obj === options.target) return;
-        obj.setOpacity(options.target.intersectsWithObject(obj) ? 0.5 : 1);
-      });
-    }
+    //  function onChange(options) {
+    //   options.target.setCoords();
+    //   canvas.forEachObject(function(obj) {
+    //     if (obj === options.target) return;
+    //     obj.setOpacity(options.target.intersectsWithObject(obj) ? 0.6 : 1);
+    //   });
+    // }
 
   }
 
